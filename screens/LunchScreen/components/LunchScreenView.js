7ff0c43b36d92910a09a37eb341d149b924d2460
Alex Stokes <stokesbga@gmail.com>
2019-07-25 04:52:43 -0400

    remove console log

--- screens/LunchScreen/components/LunchScreenView.js
+++ screens/LunchScreen/components/LunchScreenView.js
@@ -20,7 +20,6 @@ export default class LunchScreenView extends React.PureComponent {
   }
 
   _goToRestaurantScreen = (item) => {
-    console.log(item);
     this.props.navigation.navigate('Restaurant', item)
   }
 
